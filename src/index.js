fa4be8618f85e0fd1ea07a6a1b932a051bd0364a
a_osovsky <116560582+ArtemyOsovskoi@users.noreply.github.com>
2025-03-06 18:13:40 +0300

    display hourly weather, further styling, new html structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import "./styles.css";
 //import { someFunction } from "./moduleName.js"; <-- reminder for myself how to do it :)
 
-
-
 //TRY to USE both async await AND promises :3
 
-//take a location and return the weather data for that location
-//variable with location
 let locationInputData = document.getElementById("location");
 let locationButton = document.getElementById("location_btn");
-let todayWeatherNode = document.getElementById('today_weather');
+let todayWeatherNode = document.getElementById("today_weather");
+let todayCondition = document.getElementById("today_weather_condition");
+let hourlyNodes = document.getElementsByClassName("hourly_weather");
+
+
 
 async function getTodayWeatherData() {
   try {
@@ -19,9 +19,10 @@ async function getTodayWeatherData() {
     const jsonData = await response.json();
     if (response.ok) {
       console.log("Promise resolved");
-      console.log(jsonData);
+      console.log(jsonData.days[0]);
       todayWeatherNode.innerHTML = `Today in ${locationInputData.value} is ${jsonData.days[0].temp}°C`;
-      
+      todayCondition.innerHTML = jsonData.days[0].conditions;
+
       return jsonData;
     } else {
       console.error("Promise resolved but HTTP status failed");
@@ -31,50 +32,39 @@ async function getTodayWeatherData() {
   }
 }
 
+
 async function getHourlyWeatherData() {
-    try {
-        let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationInputData.value}?unitGroup=metric&key=N4YPZMGEYDWUN5QDN4464VYV9&contentType=json`;
-    
-        const response = await fetch(url);
-        const jsonData = await response.json();
+  try {
+    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationInputData.value}?unitGroup=metric&key=N4YPZMGEYDWUN5QDN4464VYV9&contentType=json`;
 
+    const response = await fetch(url);
+    const jsonData = await response.json();
 
-        if (response.ok) {
-          console.log("Promise resolved");
-          todayWeatherNode.innerHTML = `${jsonData.days[0].hours.temp}`;
-          
-          return jsonData;
-        } else {
-          console.error("Promise resolved but HTTP status failed");
-        }
-      } catch {
-        console.error("Promise rejected");
+    if (response.ok) {
+      console.log("Promise resolved");
+
+      const hourlyArr = jsonData.days[0].hours;
+      hourlyArr.splice(0, 8);
+
+      const tempHourlyArr = hourlyArr.map((value) => value.temp);
+      console.log(`hourly array ${tempHourlyArr}`);
+
+      for (let index = 0; index < tempHourlyArr.length; index++) {
+        hourlyNodes[index].innerHTML += tempHourlyArr[index] + "°C";
       }
+    } else {
+      console.error("Promise resolved but HTTP status failed");
+    }
+  } catch {
+    console.error("Promise rejected");
+  }
 }
 
-//Проблема - Нужно взять данные из массива hours.temp - то есть
-/* пройтись по массиву и взять нужные данные
-потом отобразить эти данные в параграфах для
-каждого часа в сутках 
-array.map мб подойдет? По сути нужно назначить для
-кадого параграфа.innerHTML данные из каждого hours.temp
-*/
+async function getWeeklyWeatherData() {
 
-
-
-
+}
 
 
 locationButton.addEventListener("click", getTodayWeatherData);
-
-//DOM
-/* const todayWeatherDiv = document.getElementById('today_weather');
-
-async function todayWeatherDOM(data) {
-    //let weatherData = await jsonData;
-    const todayWeatherContent = document.createTextNode(data.days[0].feelslike);
-    
-    todayWeatherDiv.appendChild(todayWeatherContent);
-}
-
- */
+locationButton.addEventListener("click", getHourlyWeatherData);
+locationButton.addEventListener("click", getWeeklyWeatherData);
