d8f4e746b61d42ee77bea43f00a5f14a8ac6bf37
a_osovsky <116560582+ArtemyOsovskoi@users.noreply.github.com>
2025-03-11 18:37:13 +0300

    dialog window

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./styles.css";
 import { getTodayWeatherData } from "./weatherData";
 import { getHourlyWeatherData } from "./weatherData";
 import { getWeeklyWeatherData } from "./weatherData";
+import { switchTemp } from "./switchFunc";
 
 let locationButton = document.getElementById("location_btn");
 locationButton.addEventListener("click", getTodayWeatherData);
@@ -13,3 +14,26 @@ locationButton.addEventListener("click", getWeeklyWeatherData);
   add those 2 buttons inside one menu button
   and then finish styling
 */
+let menuButton = document.getElementById("menu");
+let dialog = document.getElementById("dialog");
+menuButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+let closeMenu = document.getElementById("closeButton");
+closeMenu.addEventListener("click", () => {
+  dialog.close();
+})
+
+/* //show form window - input the details for the new book
+const showFormWindow = document.getElementById("formPopup");
+const dialog = document.getElementById("dialog");
+showFormWindow.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+//close form window
+const closeButton = document.getElementById("closeButton");
+closeButton.addEventListener("click", () => {
+  dialog.close();
+}); */
